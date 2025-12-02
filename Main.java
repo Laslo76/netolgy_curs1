561c702f4d9c335826cbf3225806a1ad2a0f7776
Laslo76 <162439161+Laslo76@users.noreply.github.com>
2025-12-02 09:43:47 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+package ru.netology.graphics;
+
+import ru.netology.graphics.image.ColorsSchema;
+import ru.netology.graphics.image.Converter;
+import ru.netology.graphics.image.TextGraphicsConverter;
+import ru.netology.graphics.server.GServer;
+
+import java.io.File;
+import java.io.PrintWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Converter converter = new Converter("");
+
+        GServer server = new GServer(converter); // Создаём объект сервера
+        server.start(); // Запускаем
+
+        //Или то же, но с выводом на экран:
+        //String url = "https://raw.githubusercontent.com/netology-code/java-diplom/main/pics/simple-test.png";
+        //String imgTxt = converter.convert(url);
+        //System.out.println(imgTxt);
+    }
+}
